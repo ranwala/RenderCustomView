2f89738133013dc4302ea5b8fc6d22346757927e
Chandu Ranwala <Chandu@CreativeSoftware.com>
2019-09-28 22:27:27 +0530

    added custom view

---
+++ RenderCustomView/RenderCustomView/SharedViews/CustomView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RenderCustomView.SharedViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomView
+    {
+        public static readonly BindableProperty NameProperty = BindableProperty.Create(
+            "Name",
+            typeof(string),
+            typeof(CustomView),
+            "",
+            BindingMode.TwoWay,
+            propertyChanged: OnNamePropertyChanged);
+
+        public CustomView()
+        {
+            InitializeComponent();
+        }
+
+        public string Name
+        {
+            get => GetValue(NameProperty).ToString();
+            set => SetValue(NameProperty, value);
+        }
+
+        private static void OnNamePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var label = (CustomView) bindable;
+            label.name.Text = newValue.ToString();
+        }
+    }
+}
